1486cd91b7ee9e18b6b2d0dabd1c3a33a7bd67e4
heinricov <heinricov.github@gmail.com>
2023-07-18 09:43:02 +0700

    edit-hero-1

--- src/components/MyHero.jsx
+++ src/components/MyHero.jsx
@@ -10,7 +10,7 @@ export default function MyHero() {
               Hallo,,
             </h1>
             <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-              I'm HEINRICOV
+              I'm HEINRICOV AD
             </h1>
             <h3 className="font-bold mb-6">Web Developer | Frontend</h3>
             <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
