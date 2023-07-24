bbdeb68b2d7f9487c95bd637ad53af249a1760eb
heinricov <heinricov.github@gmail.com>
2023-07-24 18:04:32 +0700

    ganti navbar lagi

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
+import MeNavbar from "./components/MeNavbar";
 import MyAbout from "./components/MyAbout";
 import MyContact from "./components/MyContact";
 import MyFooter from "./components/MyFooter";
 import MyHero from "./components/MyHero";
-import MyNafigasi from "./components/MyNafigasi";
+// import MyNafigasi from "./components/MyNafigasi";
 import MyPortfolio from "./components/MyPortfolio";
 import MySkill from "./components/MySkill";
 
 function App() {
   return (
     <>
-      <MyNafigasi />
+      {/* <MyNafigasi /> */}
+      <MeNavbar />
       <MyHero />
       <MyAbout />
       <MySkill />
