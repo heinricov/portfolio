a7d777aeaed137aef75747ee1e744e7442cecf8f
heinricov <heinricov.github@gmail.com>
2023-07-25 11:35:22 +0700

    solusi dari discord

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,11 @@ import MyHero from "./components/MyHero";
 import MyNafigasi from "./components/MyNafigasi";
 import MyPortfolio from "./components/MyPortfolio";
 import MySkill from "./components/MySkill";
-// import MeNavbar from "./components/MeNavbar";
 
 function App() {
   return (
     <>
       <MyNafigasi />
-      {/* <MeNavbar /> */}
       <MyHero />
       <MyAbout />
       <MySkill />
